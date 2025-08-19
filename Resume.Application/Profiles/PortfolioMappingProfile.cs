da5a34de4277f4dade39afcd7990247814432aef
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-19 17:51:59 +0330

    map portfolio and portfolio category models to their view models by auto mapper

---
+++ Resume.Application/Profiles/PortfolioMappingProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Resume.Domain.Models;
+using Resume.Domain.ViewModels.Portfolio;
+
+namespace Resume.Application.Profiles;
+
+public class PortfolioMappingProfile: Profile
+{
+    public PortfolioMappingProfile()
+    {
+        #region Portfolio
+
+        CreateMap<Portfolio, CreatePortfolioViewModel>().ReverseMap();
+        CreateMap<Portfolio, EditPortfolioViewModel>().ReverseMap();
+        CreateMap<Portfolio, DeletePortfolioViewModel>().ReverseMap();
+        CreateMap<Portfolio, PortfolioViewModel>().ReverseMap();
+        CreateMap<Portfolio, PortfolioListViewModel>().ReverseMap();
+
+        #endregion
+
+        #region Portfolio Category
+
+        CreateMap<PortfolioCategory, CreatePortfolioCategoryViewModel>().ReverseMap();
+        CreateMap<PortfolioCategory, EditPortfolioCategoryViewModel>().ReverseMap();
+        CreateMap<PortfolioCategory, DeletePortfolioCategoryViewModel>().ReverseMap();
+        CreateMap<PortfolioCategory, PortfolioCategoryViewModel>().ReverseMap();
+        CreateMap<PortfolioCategory, PortfolioCategoryListViewModel>().ReverseMap();
+
+        #endregion
+
+    }
+}
