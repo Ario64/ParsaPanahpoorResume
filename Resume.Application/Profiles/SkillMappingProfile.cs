b280aac17afce52f1120d53a87d7200c1423c921
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-19 17:56:59 +0330

    map skill to its view models by aito mapper

---
+++ Resume.Application/Profiles/SkillMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Resume.Domain.Models;
+using Resume.Domain.ViewModels.Skill;
+
+namespace Resume.Application.Profiles;
+
+public class SkillMappingProfile : Profile
+{
+    public SkillMappingProfile()
+    {
+        CreateMap<Skill, SkillViewModel>().ReverseMap();
+        CreateMap<Skill, CreateSkillViewModel>().ReverseMap();
+        CreateMap<Skill, EditSkillViewModel>().ReverseMap();
+        CreateMap<Skill, DeleteSkillViewModel>().ReverseMap();
+        CreateMap<Skill, SkillListViewModel>().ReverseMap();
+    }
+}
