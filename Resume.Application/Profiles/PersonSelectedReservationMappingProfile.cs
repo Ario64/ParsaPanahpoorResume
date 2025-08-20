08e3639ea1dbbf6298522f084c42e622e5a46d13
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-20 15:54:15 +0330

    map person selected reservation models to its view models using auto mapper

---
+++ Resume.Application/Profiles/PersonSelectedReservationMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Resume.Domain.Entity.Reservation;
+using Resume.Domain.ViewModels.PersonSelectedReservation;
+
+namespace Resume.Application.Profiles;
+
+public class PersonSelectedReservationMappingProfile : Profile
+{
+    public PersonSelectedReservationMappingProfile()
+    {
+        CreateMap<PersonSelectedReservation, PersonSelectedReservationViewModel>().ReverseMap();
+        CreateMap<PersonSelectedReservation, PersonSelectedReservationListViewModel>().ReverseMap();
+        CreateMap<PersonSelectedReservation, CreatePersonSelectedReservationViewModel>().ReverseMap();
+        CreateMap<PersonSelectedReservation, EditPersonSelectedReservationViewModel>().ReverseMap();
+        CreateMap<PersonSelectedReservation, DeletePersonSelectedReservationViewModel>().ReverseMap();
+    }
+}
