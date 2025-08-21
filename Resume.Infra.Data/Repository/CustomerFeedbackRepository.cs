6ac9c6e210dc2148b8f3c1fdcdd9565b2d17239f
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-21 18:47:25 +0330

    implement customer feedback repository

---
+++ Resume.Infra.Data/Repository/CustomerFeedbackRepository.cs
@@ -0,0 +1,21 @@
+﻿using Resume.Domain.Entity;
+using Resume.Domain.IRepository.CustomerFeedback;
+using Resume.Infra.Data.Context;
+
+namespace Resume.Infra.Data.Repository;
+
+public class CustomerFeedbackRepository : GenericRepository<CustomerFeedback>, ICustomerFeedbackRepository
+{
+    #region ctor
+
+    private readonly AppDbContext _context;
+
+    public CustomerFeedbackRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    #endregion
+
+
+}
