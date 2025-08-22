e844e58a74c3d7b1030f1db23f92c69dd6d03404
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-22 13:18:00 +0330

    implement reservation date repository

---
+++ Resume.Infra.Data/Repository/ReservationDateRepository.cs
@@ -0,0 +1,20 @@
+﻿using Resume.Domain.Entity;
+using Resume.Domain.IRepository.ReservationDate;
+using Resume.Infra.Data.Context;
+
+namespace Resume.Infra.Data.Repository;
+
+public class ReservationDateRepository : GenericRepository<ReservationDate>, IReservationDateRepository
+{
+    #region ctor
+
+    private readonly AppDbContext _context;
+
+    public ReservationDateRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    #endregion
+
+}
