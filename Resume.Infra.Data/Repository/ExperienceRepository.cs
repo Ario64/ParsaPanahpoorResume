306da96caa056b040f36e2f129b41cc941de9ac2
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-22 13:05:37 +0330

    implement experience repository

---
+++ Resume.Infra.Data/Repository/ExperienceRepository.cs
@@ -0,0 +1,20 @@
+﻿using Resume.Domain.Entity;
+using Resume.Domain.IRepository.Experience;
+using Resume.Infra.Data.Context;
+
+namespace Resume.Infra.Data.Repository;
+
+public class ExperienceRepository : GenericRepository<Experience>, IExperienceRepository
+{
+    #region ctor
+
+    private readonly AppDbContext _context;
+
+    public ExperienceRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    #endregion
+
+}
