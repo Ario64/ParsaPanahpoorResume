3e80a9467e8e268196dac9bc5d96625a577e30a0
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-22 13:23:08 +0330

    implement skill repository

---
+++ Resume.Infra.Data/Repository/SkillRepository.cs
@@ -0,0 +1,20 @@
+﻿using Resume.Domain.IRepository.Skill;
+using Resume.Domain.Models;
+using Resume.Infra.Data.Context;
+
+namespace Resume.Infra.Data.Repository;
+
+public class SkillRepository : GenericRepository<Skill>, ISkillRepository
+{
+    #region ctor
+
+    private readonly AppDbContext _context;
+
+    public SkillRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    #endregion
+
+}
