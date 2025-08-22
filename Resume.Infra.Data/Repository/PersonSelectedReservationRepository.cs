a9e12a3262284919ca76942560d8502153e65e77
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-22 13:13:53 +0330

    implement person selected reservation repository

---
+++ Resume.Infra.Data/Repository/PersonSelectedReservationRepository.cs
@@ -0,0 +1,20 @@
+﻿using Resume.Domain.Entity.Reservation;
+using Resume.Domain.IRepository.PersonSelectedReservation;
+using Resume.Infra.Data.Context;
+
+namespace Resume.Infra.Data.Repository;
+
+public class PersonSelectedReservationRepository : GenericRepository<PersonSelectedReservation>, IPersonSelectedReservation
+{
+    #region ctor
+
+    private readonly AppDbContext _context;
+
+    public PersonSelectedReservationRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    #endregion
+
+}
