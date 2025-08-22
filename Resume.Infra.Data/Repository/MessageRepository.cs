0f0a0a9d2d191b90d0705bae07757dbebf2a076e
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-22 13:11:47 +0330

    implement message repository

---
+++ Resume.Infra.Data/Repository/MessageRepository.cs
@@ -0,0 +1,20 @@
+﻿using Resume.Domain.IRepository.Message;
+using Resume.Domain.Models;
+using Resume.Infra.Data.Context;
+
+namespace Resume.Infra.Data.Repository;
+
+public class MessageRepository : GenericRepository<Message>, IMessageRepository
+{
+    #region ctor
+
+    private readonly AppDbContext _context;
+
+    public MessageRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    #endregion
+
+}
