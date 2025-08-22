eb458db5c80e0322b2ac410e5de32a80802a2238
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-22 13:26:57 +0330

    implement thing i do repository

---
+++ Resume.Infra.Data/Repository/ThingIDoRepository.cs
@@ -0,0 +1,20 @@
+﻿using Resume.Domain.Entity;
+using Resume.Domain.IRepository.ThingIDo;
+using Resume.Infra.Data.Context;
+
+namespace Resume.Infra.Data.Repository;
+
+public class ThingIDoRepository : GenericRepository<ThingIDo>, IThingIDoRepository
+{
+    #region ctor
+
+    private readonly AppDbContext _context;
+
+    public ThingIDoRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    #endregion
+
+}
