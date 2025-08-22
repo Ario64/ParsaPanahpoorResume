7b3e4d9e8bdabce60ce2bf1e903d921388bef04d
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-22 13:16:21 +0330

    implement portfolio repository

---
+++ Resume.Infra.Data/Repository/PortfolioRepository.cs
@@ -0,0 +1,21 @@
+﻿using Resume.Domain.IRepository.Portfolio;
+using Resume.Domain.Models;
+using Resume.Infra.Data.Context;
+
+namespace Resume.Infra.Data.Repository;
+
+public class PortfolioRepository : GenericRepository<Portfolio> , IPortfolioRepository
+{
+    #region ctor
+
+    private readonly AppDbContext _context;
+
+    public PortfolioRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    #endregion
+
+
+}
