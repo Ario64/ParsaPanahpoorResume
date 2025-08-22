d5832bd0201da980247471f06f0458e2107c9c93
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-22 13:25:19 +0330

    implement social media repository

---
+++ Resume.Infra.Data/Repository/SocialMediaRepository.cs
@@ -0,0 +1,21 @@
+﻿using Resume.Domain.Entity;
+using Resume.Domain.IRepository.SocialMedia;
+using Resume.Infra.Data.Context;
+
+namespace Resume.Infra.Data.Repository;
+
+public class SocialMediaRepository : GenericRepository<SocialMedia>, ISocialMediaRepository
+{
+    #region ctor
+
+    private readonly AppDbContext _context;
+
+    public SocialMediaRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    #endregion
+
+
+}
