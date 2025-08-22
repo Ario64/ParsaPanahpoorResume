c358409e4606d232a2cce40e88e44a1d173b0ac7
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-22 13:03:11 +0330

    create customer log repository

---
+++ Resume.Infra.Data/Repository/CustomerLogoRepository.cs
@@ -0,0 +1,21 @@
+﻿using Resume.Domain.Entity;
+using Resume.Domain.IRepository.CustomerLogo;
+using Resume.Infra.Data.Context;
+
+namespace Resume.Infra.Data.Repository;
+
+public class CustomerLogoRepository : GenericRepository<CustomerLogo>, ICustomerLogoRepository
+{
+    #region ctor
+
+    private readonly AppDbContext _context;
+
+    public CustomerLogoRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    #endregion
+
+
+}
