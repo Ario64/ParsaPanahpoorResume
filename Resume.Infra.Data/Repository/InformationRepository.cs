f1838e8d0dfb1eef614940622761287acd88933f
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-22 13:07:31 +0330

    implement information repository

---
+++ Resume.Infra.Data/Repository/InformationRepository.cs
@@ -0,0 +1,20 @@
+﻿using Resume.Domain.IRepository.Information;
+using Resume.Domain.Models;
+using Resume.Infra.Data.Context;
+
+namespace Resume.Infra.Data.Repository;
+
+public class InformationRepository : GenericRepository<Information>, IInformationRepository
+{
+    #region ctor
+
+    private readonly AppDbContext _context;
+
+    public InformationRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    #endregion
+
+}
