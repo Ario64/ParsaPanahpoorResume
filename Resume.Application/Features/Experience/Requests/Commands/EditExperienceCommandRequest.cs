a524c6f6b1d2085fe5bb8b86763f8aa923d7f4bf
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-27 18:27:36 +0330

    implement experience update

---
+++ Resume.Application/Features/Experience/Requests/Commands/EditExperienceCommandRequest.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Resume.Domain.ViewModels.Experience;
+
+namespace Resume.Application.Features.Experience.Requests.Commands;
+
+public record EditExperienceCommandRequest : IRequest<Unit>
+{
+    public ulong Id { get; init; }
+    public EditExperienceViewModel  EditExperienceViewModel { get; init; }
+}
