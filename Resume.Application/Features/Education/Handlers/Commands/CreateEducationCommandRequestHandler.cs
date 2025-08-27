c6dfd1bb2271c06d0ca2844f57707ccd3588f0bd
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-27 16:10:18 +0330

    create education using command and handler

---
+++ Resume.Application/Features/Education/Handlers/Commands/CreateEducationCommandRequestHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using Resume.Application.Features.Education.Requests.Commands;
+using Resume.Application.UnitOfWork;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Resume.Application.Features.Education.Handlers.Commands;
+
+public class CreateEducationCommandRequestHandler : IRequestHandler<CreateEducationCommandRequest, Unit>
+{
+    #region Constructor
+
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public CreateEducationCommandRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    #endregion
+
+    public async Task<Unit> Handle(CreateEducationCommandRequest request, CancellationToken cancellationToken)
+    {
+        var education = _mapper.Map<Resume.Domain.Entity.Education>(request.CreateEducationViewModel);
+        _unitOfWork.GenericRepository<Resume.Domain.Entity.Education>().Add(education);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+        return Unit.Value;
+    }
+}
---
+++ Resume.Application/Features/Education/Requests/Commands/CreateEducationCommandRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Resume.Domain.ViewModels.Education;
+
+namespace Resume.Application.Features.Education.Requests.Commands;
+
+public record CreateEducationCommandRequest : IRequest<Unit>
+{
+    public CreateEducationViewModel CreateEducationViewModel { get; set; }
+}
