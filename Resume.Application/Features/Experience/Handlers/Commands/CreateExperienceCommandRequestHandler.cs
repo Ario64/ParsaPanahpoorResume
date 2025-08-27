e25285bbe7b5d698139fc46e0d830de6447ea8a2
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-27 18:13:40 +0330

    implemet create experience

---
+++ Resume.Application/Features/Experience/Handlers/Commands/CreateExperienceCommandRequestHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using Resume.Application.Features.Experience.Requests.Commands;
+using Resume.Application.UnitOfWork;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Resume.Application.Features.Experience.Handlers.Commands;
+
+public class CreateExperienceCommandRequestHandler : IRequestHandler<CreateExperienceCommandRequest, Unit>
+{
+    #region Constructor
+
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public CreateExperienceCommandRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;   
+        _mapper = mapper;
+    }
+
+    #endregion
+
+    public async Task<Unit> Handle(CreateExperienceCommandRequest request, CancellationToken cancellationToken)
+    {
+        var experinece = _mapper.Map<Resume.Domain.Entity.Experience>(request.CreateExperienceViewModel);
+        _unitOfWork.GenericRepository<Resume.Domain.Entity.Experience>().Add(experinece);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+        return Unit.Value;
+    }
+}
---
+++ Resume.Application/Features/Experience/Requests/Commands/CreateExperienceCommandRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Resume.Domain.ViewModels.Experience;
+
+namespace Resume.Application.Features.Experience.Requests.Commands;
+
+public record CreateExperienceCommandRequest : IRequest<Unit>
+{
+    public CreateExperienceViewModel CreateExperienceViewModel { get; init; }
+}
