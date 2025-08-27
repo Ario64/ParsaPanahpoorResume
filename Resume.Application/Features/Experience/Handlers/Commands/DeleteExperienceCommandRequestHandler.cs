befaeed5a3297a5556ae03cb9b856861d6aa9e1c
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-27 19:00:10 +0330

    implement delete experience

---
+++ Resume.Application/Features/Experience/Handlers/Commands/DeleteExperienceCommandRequestHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using Resume.Application.Features.Experience.Requests.Commands;
+using Resume.Application.UnitOfWork;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Resume.Application.Features.Experience.Handlers.Commands;
+
+public class DeleteExperienceCommandRequestHandler : IRequestHandler<DeleteExperienceCommandRequest, Unit>
+{
+    #region Constructor
+
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public DeleteExperienceCommandRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    #endregion
+
+    public async Task<Unit> Handle(DeleteExperienceCommandRequest request, CancellationToken cancellationToken)
+    {
+        var experience = await _unitOfWork.GenericRepository<Resume.Domain.Entity.Experience>().GetAsync(request.Id, cancellationToken);
+        _unitOfWork.GenericRepository<Resume.Domain.Entity.Experience>().Delete(experience);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+        return Unit.Value;
+    }
+}
---
+++ Resume.Application/Features/Experience/Requests/Commands/DeleteExperienceCommandRequest.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Resume.Application.Features.Experience.Requests.Commands;
+
+public record DeleteExperienceCommandRequest : IRequest<Unit>
+{
+    public ulong Id { get; set; }
+}
