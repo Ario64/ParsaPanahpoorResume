60cdfc32beaab15d5f7efc48146830a862db2bae
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-28 17:05:21 +0330

    implement delete message command and handler

---
+++ Resume.Application/Features/Message/Handlers/Commands/EditMessageCommandRequestHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using Resume.Application.Features.Message.Requests.Commands;
+using Resume.Application.UnitOfWork;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Resume.Application.Features.Message.Handlers.Commands;
+
+public class EditMessageCommandRequestHandler : IRequestHandler<EditMessageCommandRequest, Unit>
+{
+    #region Constructor
+
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public EditMessageCommandRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    #endregion
+
+    public async Task<Unit> Handle(EditMessageCommandRequest request, CancellationToken cancellationToken)
+    {
+        var message = await _unitOfWork.GenericRepository<Resume.Domain.Entity.Message>().GetAsync(request.Id, cancellationToken);
+        _mapper.Map(request.EditMessageViewModel, message);
+        _unitOfWork.GenericRepository<Resume.Domain.Entity.Message>().Update(message);
+        return Unit.Value;
+    }
+}
---
+++ Resume.Application/Features/Message/Requests/Commands/EditMessageCommandRequest.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Resume.Domain.ViewModels.Message;
+
+namespace Resume.Application.Features.Message.Requests.Commands;
+
+public record EditMessageCommandRequest : IRequest<Unit>
+{
+    public ulong Id { get; init; }
+    public EditMessageViewModel EditMessageViewModel { get; init; }
+}
