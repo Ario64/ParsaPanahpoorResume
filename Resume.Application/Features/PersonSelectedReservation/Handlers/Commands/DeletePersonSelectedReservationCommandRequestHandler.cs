c2bf2ca3ae1d12a27d9870ae401d4aebb0f5942a
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-28 18:11:45 +0330

    implement DeletePersonSelectedReservation

---
+++ Resume.Application/Features/PersonSelectedReservation/Handlers/Commands/DeletePersonSelectedReservationCommandRequestHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using MediatR;
+using Resume.Application.Features.PersonSelectedReservation.Requests.Commands;
+using Resume.Application.UnitOfWork;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Resume.Application.Features.PersonSelectedReservation.Handlers.Commands;
+
+public class DeletePersonSelectedReservationCommandRequestHandler : IRequestHandler<DeletePersonSelectedReservationCommandRequest, Unit>
+{
+    #region Constructor
+
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public DeletePersonSelectedReservationCommandRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    #endregion
+
+    public async Task<Unit> Handle(DeletePersonSelectedReservationCommandRequest request, CancellationToken cancellationToken)
+    {
+       var person = await _unitOfWork.GenericRepository<Domain.Entity.Reservation.PersonSelectedReservation>()
+                                     .GetAsync(request.Id, cancellationToken);
+
+        _unitOfWork.GenericRepository<Domain.Entity.Reservation.PersonSelectedReservation>().Delete(person);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+        return Unit.Value;
+    }
+}
---
+++ Resume.Application/Features/PersonSelectedReservation/Requests/Commands/DeletePersonSelectedReservationCommandRequest.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Resume.Application.Features.PersonSelectedReservation.Requests.Commands;
+
+public record DeletePersonSelectedReservationCommandRequest : IRequest<Unit>
+{
+    public ulong Id { get; init; }
+}
