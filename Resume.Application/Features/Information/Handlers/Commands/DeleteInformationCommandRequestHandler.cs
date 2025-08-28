d099121cbed67f84be242dd2cbf5c56316019d22
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-28 14:18:50 +0330

    implement delete information

---
+++ Resume.Application/Features/Information/Handlers/Commands/DeleteInformationCommandRequestHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Resume.Application.Features.Information.Requests.Commands;
+using Resume.Application.UnitOfWork;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Resume.Application.Features.Information.Handlers.Commands;
+
+public class DeleteInformationCommandRequestHandler : IRequestHandler<DeleteInformationCommandRequest, Unit>
+{
+    #region Constructor
+
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DeleteInformationCommandRequestHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    #endregion
+
+    public async Task<Unit> Handle(DeleteInformationCommandRequest request, CancellationToken cancellationToken)
+    {
+        var information = await _unitOfWork.GenericRepository<Resume.Domain.Entity.Information>().GetAsync(request.Id, cancellationToken);
+        _unitOfWork.GenericRepository<Resume.Domain.Entity.Information>().Delete(information);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+        return Unit.Value;
+    }
+}
---
+++ Resume.Application/Features/Information/Requests/Commands/DeleteInformationCommandRequest.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Resume.Application.Features.Information.Requests.Commands;
+
+public record DeleteInformationCommandRequest : IRequest<Unit>
+{
+    public ulong Id { get; set; }
+}
