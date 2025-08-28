adecab0610e724e3aca724cb84c52f8909963cc0
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-28 17:53:33 +0330

    create PersonSelectedReservation

---
+++ Resume.Application/Features/PersonSelectedReservation/Handlers/Commands/CreatePersonSelectedReservationCommandRequestHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using Resume.Application.Features.PersonSelectedReservation.Requests.Commands;
+using Resume.Application.UnitOfWork;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Resume.Application.Features.PersonSelectedReservation.Handlers.Commands;
+
+public class CreatePersonSelectedReservationCommandRequestHandler : IRequestHandler<CreatePersonSelectedReservationCommandRequest, Unit>
+{
+    #region Constructor
+
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public CreatePersonSelectedReservationCommandRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    #endregion
+
+    public async Task<Unit> Handle(CreatePersonSelectedReservationCommandRequest request, CancellationToken cancellationToken)
+    {
+        var person = _mapper.Map<Resume.Domain.Entity.Reservation.PersonSelectedReservation>(request.CreatePersonSelectedReservation);
+        _unitOfWork.GenericRepository<Resume.Domain.Entity.Reservation.PersonSelectedReservation>().Add(person);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+        return Unit.Value;
+    }
+}
---
+++ Resume.Application/Features/PersonSelectedReservation/Requests/Commands/CreatePersonSelectedReservationCommandRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Resume.Domain.ViewModels.PersonSelectedReservation;
+
+namespace Resume.Application.Features.PersonSelectedReservation.Requests.Commands;
+
+public record CreatePersonSelectedReservationCommandRequest : IRequest<Unit>
+{
+    public CreatePersonSelectedReservationViewModel CreatePersonSelectedReservation { get; set; }
+}
