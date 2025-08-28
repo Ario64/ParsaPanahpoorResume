29ae36d6eb38f844a0f21e88c6558fea3e50fbfc
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-28 17:14:29 +0330

    implement delete message

---
+++ Resume.Application/Features/Message/Handlers/Commands/DeleteMessageCommandRequestHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Resume.Application.Features.Message.Requests.Commands;
+using Resume.Application.UnitOfWork;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Resume.Application.Features.Message.Handlers.Commands;
+
+public class DeleteMessageCommandRequestHandler : IRequestHandler<DeleteMessageCommandRequest, Unit>
+{
+    #region Constructor
+
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DeleteMessageCommandRequestHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    #endregion
+
+    public async Task<Unit> Handle(DeleteMessageCommandRequest request, CancellationToken cancellationToken)
+    {
+        var message = await _unitOfWork.GenericRepository<Resume.Domain.Entity.Message>().GetAsync(request.Id, cancellationToken);
+        _unitOfWork.GenericRepository<Resume.Domain.Entity.Message>().Delete(message);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);  
+        return Unit.Value;
+    }
+}
---
+++ Resume.Application/Features/Message/Requests/Commands/DeleteMessageCommandRequest.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Resume.Application.Features.Message.Requests.Commands;
+
+public record DeleteMessageCommandRequest : IRequest<Unit>
+{
+    public ulong Id { get; set; }
+}
