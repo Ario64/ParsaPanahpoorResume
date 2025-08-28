d0e0440070fd62506ba0ffd465b98236abc358ee
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-28 18:01:11 +0330

    edit PersonSelectedReservation

---
+++ Resume.Application/Features/PersonSelectedReservation/Handlers/Commands/EditPersonSelectedReservationCommandRequestHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using MediatR;
+using Resume.Application.Features.PersonSelectedReservation.Requests.Commands;
+using Resume.Application.UnitOfWork;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Resume.Application.Features.PersonSelectedReservation.Handlers.Commands
+{
+    public class EditPersonSelectedReservationCommandRequestHandler : IRequestHandler<EditPersonSelectedReservationCommandRequest, Unit>
+    {
+        #region Constructor
+
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public EditPersonSelectedReservationCommandRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        #endregion
+
+        public async Task<Unit> Handle(EditPersonSelectedReservationCommandRequest request, CancellationToken cancellationToken)
+        {
+            var person = await _unitOfWork.GenericRepository<Resume.Domain.Entity.Reservation.PersonSelectedReservation>()
+                                          .GetAsync(request.Id, cancellationToken);
+
+            _mapper.Map(request.EditPersonSelectedReservationViewModel, person);
+            _unitOfWork.GenericRepository<Resume.Domain.Entity.Reservation.PersonSelectedReservation>().Update(person);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
---
+++ Resume.Application/Features/PersonSelectedReservation/Requests/Commands/EditPersonSelectedReservationCommandRequest.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Resume.Domain.ViewModels.PersonSelectedReservation;
+
+namespace Resume.Application.Features.PersonSelectedReservation.Requests.Commands;
+
+public record EditPersonSelectedReservationCommandRequest  : IRequest<Unit>
+{
+    public ulong Id { get; init; }
+    public EditPersonSelectedReservationViewModel  EditPersonSelectedReservationViewModel { get; init; }
+}
