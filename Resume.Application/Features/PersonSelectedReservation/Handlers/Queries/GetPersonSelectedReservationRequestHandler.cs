5d9e5543629bb88e7b08d01c1f72d1d25203b7de
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-28 17:33:11 +0330

    get PersonSelectedReservation

---
+++ Resume.Application/Features/PersonSelectedReservation/Handlers/Queries/GetPersonSelectedReservationRequestHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MediatR;
+using Resume.Application.Features.PersonSelectedReservation.Requests.Queries;
+using Resume.Application.UnitOfWork;
+using Resume.Domain.ViewModels.PersonSelectedReservation;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Resume.Application.Features.PersonSelectedReservation.Handlers.Queries;
+
+public class GetPersonSelectedReservationRequestHandler : IRequestHandler<GetPersonSelectedReservationRequest, PersonSelectedReservationViewModel>
+{
+    #region Constructor
+
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public GetPersonSelectedReservationRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    #endregion
+
+    public async Task<PersonSelectedReservationViewModel> Handle(GetPersonSelectedReservationRequest request, CancellationToken cancellationToken)
+    {
+        var person = await _unitOfWork.GenericRepository<Resume.Domain.Entity.Reservation.PersonSelectedReservation>()
+                                      .GetAsync(request.Id, cancellationToken);
+
+        return _mapper.Map<PersonSelectedReservationViewModel>(person);
+    }
+}
---
+++ Resume.Application/Features/PersonSelectedReservation/Requests/Queries/GetPersonSelectedReservationRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Resume.Domain.ViewModels.PersonSelectedReservation;
+
+namespace Resume.Application.Features.PersonSelectedReservation.Requests.Queries;
+
+public record GetPersonSelectedReservationRequest : IRequest<PersonSelectedReservationViewModel>
+{
+    public ulong Id { get; init; }
+}
