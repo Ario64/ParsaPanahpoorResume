bbdd55e8648227ee39979a38047e48269d9cd9ad
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-29 15:13:57 +0330

    implement get list of portfolio

---
+++ Resume.Application/Features/Portfolio/Handlers/Queries/GetPortfolioListRequestHandler.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using MediatR;
+using Resume.Application.Features.Portfolio.Requests.Queries;
+using Resume.Application.UnitOfWork;
+using Resume.Domain.ViewModels.Pagination;
+using Resume.Domain.ViewModels.Portfolio;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Resume.Application.Features.Portfolio.Handlers.Queries;
+
+public class GetPortfolioListRequestHandler : IRequestHandler<GetPortfolioListRequest, PagedResult<PortfolioViewModel>>
+{
+    #region Constructor
+
+    private readonly IMapper _mapper;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public GetPortfolioListRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    #endregion
+
+    public async Task<PagedResult<PortfolioViewModel>> Handle(GetPortfolioListRequest request, CancellationToken cancellationToken)
+    {
+        var portfolioList = await _unitOfWork.GenericRepository<Resume.Domain.Entity.Portfolio>()
+                                             .GetAllAsync(request.page, request.pageSize, cancellationToken);
+
+        var items = _mapper.Map<IReadOnlyList<PortfolioViewModel>>(portfolioList.Items);
+
+        return new PagedResult<PortfolioViewModel>
+        {
+            Items = items,
+            Page = request.page,
+            PageSize = request.pageSize,
+            TotalCount = portfolioList.TotalCount,
+            TotalPages = portfolioList.TotalPages
+        };
+    }
+}
---
+++ Resume.Application/Features/Portfolio/Requests/Queries/GetPortfolioListRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Resume.Domain.ViewModels.Pagination;
+using Resume.Domain.ViewModels.Portfolio;
+
+namespace Resume.Application.Features.Portfolio.Requests.Queries;
+
+public record GetPortfolioListRequest(int page = 1, int pageSize = 10) : IRequest<PagedResult<PortfolioViewModel>>
+{
+}
