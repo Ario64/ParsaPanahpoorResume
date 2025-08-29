ec9bd2708c38c1bb760ac122e31e5053da2e524d
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-08-29 15:19:56 +0330

    implement create porfolio

---
+++ Resume.Application/Features/Portfolio/Handlers/Commands/CreatePortfolioCommandRequestHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using Resume.Application.Features.Portfolio.Requests.Commands;
+using Resume.Application.UnitOfWork;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Resume.Application.Features.Portfolio.Handlers.Commands;
+
+public class CreatePortfolioCommandRequestHandler : IRequestHandler<CreatePortfolioCommandRequest, Unit>
+{
+    #region Constructor
+
+    private readonly IMapper _mapper;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreatePortfolioCommandRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    #endregion
+
+    public async Task<Unit> Handle(CreatePortfolioCommandRequest request, CancellationToken cancellationToken)
+    {
+        var porfolio = _mapper.Map<Resume.Domain.Entity.Portfolio>(request.CreatePortfolioViewModel);
+        _unitOfWork.GenericRepository<Resume.Domain.Entity.Portfolio>().Add(porfolio);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+        return Unit.Value;
+    }
+}
---
+++ Resume.Application/Features/Portfolio/Requests/Commands/CreatePortfolioCommandRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Resume.Domain.ViewModels.Portfolio;
+
+namespace Resume.Application.Features.Portfolio.Requests.Commands;
+
+public record CreatePortfolioCommandRequest : IRequest<Unit>
+{
+    public CreatePortfolioViewModel CreatePortfolioViewModel { get; init; }
+}
