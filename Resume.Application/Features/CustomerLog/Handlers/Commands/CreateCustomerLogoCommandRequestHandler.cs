8a8871ed1601837c292cb67601684b5300891d22
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-09-04 16:59:56 +0330

    Add caching for newly created CustomerLogo entities
    Introduce ICacheServices dependency in CreateCustomerLogoCommandRequestHandler.
    Update the Handle method to cache CustomerLogoViewModel instances
    after creation, improving performance by reducing database fetches.

--- Resume.Application/Features/CustomerLog/Handlers/Commands/CreateCustomerLogoCommandRequestHandler.cs
+++ Resume.Application/Features/CustomerLog/Handlers/Commands/CreateCustomerLogoCommandRequestHandler.cs
@@ -1,8 +1,11 @@
 ﻿using AutoMapper;
 using MediatR;
 using Resume.Application.Features.CustomerLog.Requests.Commands;
+using Resume.Application.ICacheService;
 using Resume.Application.UnitOfWork;
 using Resume.Domain.Entity;
+using Resume.Domain.ViewModels.CustomerLogo;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,11 +17,13 @@ public class CreateCustomerLogoCommandRequestHandler : IRequestHandler<CreateCus
 
     private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
+    private readonly ICacheServices _cache;
 
-    public CreateCustomerLogoCommandRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    public CreateCustomerLogoCommandRequestHandler(IUnitOfWork unitOfWork, IMapper mapper, ICacheServices cache)
     {
         _unitOfWork = unitOfWork;
         _mapper = mapper;
+        _cache = cache; 
     }
 
     #endregion
@@ -28,6 +33,12 @@ public class CreateCustomerLogoCommandRequestHandler : IRequestHandler<CreateCus
         var customerLogo = _mapper.Map<CustomerLogo>(request.CustomerLogoViewModel);
         _unitOfWork.GenericRepository<CustomerLogo>().Add(customerLogo);
         await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        // Cache the newly created CustomerLogo
+        var cacheKey = $"CustomerLogo:{customerLogo.Id}";
+        var cachedCustomerLogo =  _mapper.Map<CustomerLogoViewModel>(customerLogo);
+        await _cache.SetAsync<CustomerLogoViewModel>(cacheKey, cachedCustomerLogo, TimeSpan.FromMinutes(10));
+
         return true;
     }
 }
