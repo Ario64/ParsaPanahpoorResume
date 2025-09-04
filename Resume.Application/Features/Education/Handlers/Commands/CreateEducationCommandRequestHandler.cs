9d7a0559941fe93ef2029bfab2d906af319eb6c0
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-09-04 17:58:51 +0330

    Add caching mechanism to CreateEducationCommandRequestHandler
    Introduce ICacheServices dependency for caching education data.
    Update constructor to include caching service and implement
    Redis caching after saving education entity with a 10-minute
    expiration time.

--- Resume.Application/Features/Education/Handlers/Commands/CreateEducationCommandRequestHandler.cs
+++ Resume.Application/Features/Education/Handlers/Commands/CreateEducationCommandRequestHandler.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using MediatR;
 using Resume.Application.Features.Education.Requests.Commands;
+using Resume.Application.ICacheService;
 using Resume.Application.UnitOfWork;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,11 +14,13 @@ public class CreateEducationCommandRequestHandler : IRequestHandler<CreateEducat
 
     private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
+    private readonly ICacheServices _cache;
 
-    public CreateEducationCommandRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    public CreateEducationCommandRequestHandler(IUnitOfWork unitOfWork, IMapper mapper, ICacheServices cache)
     {
         _unitOfWork = unitOfWork;
         _mapper = mapper;
+        _cache = cache;
     }
 
     #endregion
@@ -27,6 +30,12 @@ public class CreateEducationCommandRequestHandler : IRequestHandler<CreateEducat
         var education = _mapper.Map<Resume.Domain.Entity.Education>(request.CreateEducationViewModel);
         _unitOfWork.GenericRepository<Resume.Domain.Entity.Education>().Add(education);
         await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        //Set data in redis cache
+        var cacheKey = $"Education:{education.Id}";
+        var educationViewModel = _mapper.Map<Domain.ViewModels.Education.EducationViewModel>(education);
+        await _cache.SetAsync(cacheKey, educationViewModel, System.TimeSpan.FromMinutes(10));
+
         return true;
     }
 }
