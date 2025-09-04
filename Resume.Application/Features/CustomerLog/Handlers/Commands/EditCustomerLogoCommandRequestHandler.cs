57f9aaae8aaea6336bdaeeb5fdd196aa3e493b42
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-09-04 17:23:18 +0330

    Add caching to EditCustomerLogoCommandRequestHandler
    Introduce ICacheServices dependency to cache updated customer logo data. Implement caching logic to store the logo for 10 minutes, improving performance by reducing database fetches.

--- Resume.Application/Features/CustomerLog/Handlers/Commands/EditCustomerLogoCommandRequestHandler.cs
+++ Resume.Application/Features/CustomerLog/Handlers/Commands/EditCustomerLogoCommandRequestHandler.cs
@@ -1,8 +1,11 @@
 ﻿using AutoMapper;
 using MediatR;
 using Resume.Application.Features.CustomerLog.Requests.Commands;
+using Resume.Application.ICacheService;
 using Resume.Application.UnitOfWork;
 using Resume.Domain.Entity;
+using Resume.Domain.ViewModels.CustomerLogo;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,11 +17,13 @@ public class EditCustomerLogoCommandRequestHandler : IRequestHandler<EditCustome
 
     private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
+    private readonly ICacheServices _cache;
 
-    public EditCustomerLogoCommandRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    public EditCustomerLogoCommandRequestHandler(IUnitOfWork unitOfWork, IMapper mapper, ICacheServices cache)
     {
         _unitOfWork = unitOfWork;
         _mapper = mapper;
+        _cache = cache; 
     }
 
     #endregion
@@ -29,6 +34,11 @@ public class EditCustomerLogoCommandRequestHandler : IRequestHandler<EditCustome
         _mapper.Map(request.EditCustomerLogoViewModel, customerLogo);
         _unitOfWork.GenericRepository<CustomerLogo>().Update(customerLogo);
         await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        var cacheKey = $"CustomerLog:{customerLogo.Id}";
+        var cachedCustomerLogo = _mapper.Map<CustomerLogoViewModel>(customerLogo);
+        await _cache.SetAsync<CustomerLogoViewModel>(cacheKey, cachedCustomerLogo, TimeSpan.FromMinutes(10));
+
         return true;
     }
 }
