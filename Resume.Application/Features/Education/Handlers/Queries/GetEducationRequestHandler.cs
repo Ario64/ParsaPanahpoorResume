48c343fbd25beda83eb08925c08b6d6cf357662a
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-09-04 17:34:39 +0330

    Add caching mechanism to GetEducationRequestHandler
    Implemented caching in GetEducationRequestHandler to improve performance. Introduced ICacheServices dependency to the constructor, allowing the handler to retrieve cached education data before querying the database. If cached data is unavailable, it fetches from the database, maps it to EducationViewModel, and stores it in the cache for future requests, reducing database calls.

--- Resume.Application/Features/Education/Handlers/Queries/GetEducationRequestHandler.cs
+++ Resume.Application/Features/Education/Handlers/Queries/GetEducationRequestHandler.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
 using MediatR;
 using Resume.Application.Features.Education.Requests.Queries;
+using Resume.Application.ICacheService;
 using Resume.Application.UnitOfWork;
 using Resume.Domain.ViewModels.Education;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,20 +16,37 @@ public class GetEducationRequestHandler : IRequestHandler<GetEducationRequest, E
 
     private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
+    private readonly ICacheServices _cache;
 
-    public GetEducationRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    public GetEducationRequestHandler(IUnitOfWork unitOfWork, IMapper mapper, ICacheServices cache)
     {
         _mapper = mapper;
         _unitOfWork = unitOfWork;
+        _cache = cache;
     }
 
     #endregion
 
     public async Task<EducationViewModel> Handle(GetEducationRequest request, CancellationToken cancellationToken)
     {
+        var cacheKey = $"Education:{request.Id}";
+        var cachedEducation = await _cache.GetAsync<EducationViewModel>(cacheKey);
+
+        // Return cached education if available
+        if (cachedEducation != null)
+        {
+            return cachedEducation;
+        }
+
         var education = await _unitOfWork.GenericRepository<Resume.Domain.Entity.Education>()
                                          .GetAsync(request.Id, cancellationToken);
 
-        return _mapper.Map<EducationViewModel>(education);
+        var mappedEducation =  _mapper.Map<EducationViewModel>(education);
+        if (education != null)
+        {
+            await _cache.SetAsync(cacheKey, mappedEducation, TimeSpan.FromMinutes(10));
+        }
+
+        return mappedEducation;
     }
 }
