d7f3951492fc666142a50391292437ba3e2f3125
Ghasem Shahrokhi <shahrokhi20@yahoo.com>
2025-09-20 16:16:44 +0330

    add custom response  to application layer

---
+++ Resume.Application/Responses/BaseCoomandResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Resume.Application.Responses;
+
+public class BaseCoomandResponse
+{
+    public int Id { get; set; }
+    public bool IsSuccess { get; set; }
+    public string Message { get; set; }
+    public List<string> Errors { get; set; }
+}
